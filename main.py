29bfd2ffbd83e7d5e1c3ded73dde647c6c2cae0d
kartikbudihal <kpbudihal@gmail.com>
2024-10-27 10:13:19 -0400

    Adding to Final Code

--- main.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-API_KEY = "AIzaSyBWGd2JcfCubuZ7tpw35olV3LjQUUoCK7M"
-
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-
-print(response.text)
